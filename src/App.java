a7692041922777353e9c44952e7f903525d60d64
Dalkyan <36706161+Dalkyan@users.noreply.github.com>
2018-03-15 20:06:55 +0100

    adding factorial (#1)
    merging test

--- src/App.java
+++ src/App.java
@@ -11,7 +11,9 @@ public class App {
                 System.out.println(calculator.subtract(randomGenerator.getSmallNumber(),randomGenerator.getBigNumber()));
                 System.out.println(calculator.multiply(randomGenerator.getSmallNumber(),randomGenerator.getBigNumber()));
                 System.out.println(calculator.divide(randomGenerator.getSmallNumber(),randomGenerator.getBigNumber()));
+                System.out.println(calculator.divide(randomGenerator.getSmallNumber(),randomGenerator.getBigNumber()));
+        System.out.println(calculator.factorial(randomGenerator.getSmallNumber()));
 
-            }
+    }
         }
 
